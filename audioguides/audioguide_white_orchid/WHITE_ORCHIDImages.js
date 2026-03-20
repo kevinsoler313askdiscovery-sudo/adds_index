100f55745ecf3312d5055bd50b8162a16d6974fe
kevinsoler313 <kevinsoler313@gmail.com>
2026-03-20 11:49:01 -0500

    Update until map languajes

--- audioguides/audioguide_white_orchid/WHITE_ORCHIDImages.js
+++
@@ -1,8 +0,0 @@
-const heroImages = [
-    'white_orchid_1.webp',
-    'white_orchid_2.webp',
-    'white_orchid_3.webp',
-    'white_orchid_4.webp',
-    'white_orchid_5.webp',
-    'white_orchid_6.webp'
-];
