cc8061e815edc091b87d9675801bb991ef34b687
kevinsoler313 <kevinsoler313@gmail.com>
2026-05-16 18:22:51 -0500

    Update grand palace

---
+++ audioguides/audioguide_grand_palace/GRAND_PALACEImages.js
@@ -0,0 +1,8 @@
+const heroImages = [
+    'grand_palace_1.webp',
+    'grand_palace_2.webp',
+    'grand_palace_3.webp',
+    'grand_palace_4.webp',
+    'grand_palace_5.webp',
+    'grand_palace_6.webp'
+];
