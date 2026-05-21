e2574cd2677abfaf57b810450a6b4ba5ecb46215
kevinsoler313 <kevinsoler313@gmail.com>
2026-05-20 19:06:35 -0500

    Wat ara commit

--- audioguides/audioguide_floating_wat_arun/FLOATING_WAT_ARUNImages.js
+++ audioguides/audioguide_floating_wat_arun/FLOATING_WAT_ARUNImages.js
@@ -1,6 +1,11 @@
 const heroImages = [
     'FLOATING WEB/floating_1.webp',
     'FLOATING WEB/floating_2.webp',
-    'FLOATING  WEB/floating_3.webp',
-    'FLOATING  WEB/floating_4.webp'
+    'FLOATING WEB/floating_3.webp',
+    'FLOATING WEB/floating_4.webp',
+    'FLOATING WAT ARUN WEB/wat_arun_1.jpeg',
+    'FLOATING WAT ARUN WEB/wat_arun_2.jpeg',
+    'GRAND PALACE WEB/grand_palace_1.jpeg',
+    'GRAND PALACE WEB/grand_palace_2.jpeg',
+    'GRAND PALACE WEB/grand_palace_3.jpeg'
 ];
